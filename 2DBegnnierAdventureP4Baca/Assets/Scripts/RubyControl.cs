741271bd5041bccbace4d375f44e42cec8eaaa2b
lbwhs123 <lylabaca2028@gmail.com>
2024-10-30 11:44:39 -0700

    world interations collections
    making the prefabss collectable

--- 2DBegnnierAdventureP4Baca/Assets/Scripts/RubyControl.cs
+++ 2DBegnnierAdventureP4Baca/Assets/Scripts/RubyControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RubyControl : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,18 +16,21 @@ public class RubyControl : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    { 
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime; 
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        Vector2 position = transform.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
-
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
