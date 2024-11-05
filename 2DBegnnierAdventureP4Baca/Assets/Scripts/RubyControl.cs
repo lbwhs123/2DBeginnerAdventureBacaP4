7c64eff69cc810867dc0a82aa260ea9bd4312ba5
lbwhs123 <lylabaca2028@gmail.com>
2024-11-05 11:41:06 -0800

    Enemy control
    scripting the enemies and damaged zones and maKING THEM PREFABS

--- 2DBegnnierAdventureP4Baca/Assets/Scripts/RubyControl.cs
+++ 2DBegnnierAdventureP4Baca/Assets/Scripts/RubyControl.cs
@@ -52,6 +52,15 @@ public class RubyControl : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvicible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
