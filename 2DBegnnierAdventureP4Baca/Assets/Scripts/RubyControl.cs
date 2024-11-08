f990d3ecb00711b3cc20d46f92c9b6daac771c94
lbwhs123 <lylabaca2028@gmail.com>
2024-11-07 21:13:18 -0800

    animation
    making ruby move

--- 2DBegnnierAdventureP4Baca/Assets/Scripts/RubyControl.cs
+++ 2DBegnnierAdventureP4Baca/Assets/Scripts/RubyControl.cs
@@ -7,7 +7,7 @@ public class RubyControl : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvicible = 2;
+    public float timeInvicible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -18,12 +18,15 @@ public class RubyControl : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-
+        animator = GetComponent<Animator>();
 
     }
 
@@ -32,6 +35,17 @@ public class RubyControl : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look y", lookDirection.y);
+        animator.SetFloat("speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -54,6 +68,8 @@ public class RubyControl : MonoBehaviour
     {
         if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
